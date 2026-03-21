3d6c773844dde2424b66471accea6bdcb5269f1d
Marat Omarov <mt.omarov@yandex.ru>
2026-03-21 18:00:33 +0000

    Add task implementation

--- tasks/synchronization_primitives/unbuffered_channel/unbuffered_channel.h
+++ tasks/synchronization_primitives/unbuffered_channel/unbuffered_channel.h
@@ -1,20 +1,69 @@
 #pragma once
 
 #include <optional>
+#include <mutex>
+#include <condition_variable>
+#include <stdexcept>
 
 template <class T>
 class UnbufferedChannel {
 public:
     void Send(const T& value) {
-        // Your code goes here
+        std::unique_lock<std::mutex> lock(mutex_);
+
+        if (closed_) {
+            throw std::runtime_error("send on closed channel");
+        }
+
+        cv_.wait(lock, [this]() {
+            return !slot_.has_value() || closed_;
+        });
+        if (closed_) {
+            throw std::runtime_error("send on closed channel");
+        }
+
+        slot_ = value;
+        cv_.notify_one();
+
+        cv_.wait(lock, [this]() {
+            return !slot_.has_value() || closed_;
+        });
+
+        if (slot_.has_value()) {
+            slot_ = std::nullopt;
+            throw std::runtime_error("send on closed channel");
+        }
     }
 
     std::optional<T> Recv() {
-        // Your code goes here
+        std::unique_lock<std::mutex> lock(mutex_);
+
+        cv_.wait(lock, [this]() {
+            return slot_.has_value() || closed_;
+        });
+
+        if (slot_.has_value()) {
+            T result = std::move(*slot_);
+            slot_ = std::nullopt;
+            cv_.notify_one();
+            return result;
+        }
+
         return std::nullopt;
     }
 
     void Close() {
-        // Your code goes here
+        std::unique_lock<std::mutex> lock(mutex_);
+        if (closed_) {
+            return;
+        }
+        closed_ = true;
+        cv_.notify_all();
     }
+
+private:
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::optional<T> slot_;
+    bool closed_ = false;
 };
